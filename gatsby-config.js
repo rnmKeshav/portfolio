47009a03a0175cb99b3c32f1d9808a4d8c32a8cd
Keshav Kumar <keshav.kumar@practo.com>
2021-04-18 12:15:25 +0530

    basic cursor blinker

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "",
+        trackingId: "XXXX",
       },
     },
     "gatsby-plugin-sharp",
