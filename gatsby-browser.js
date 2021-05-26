510d08dea3649b46022568e902c78f66c37718b7
Keshav Kumar <keshav.kumar@practo.com>
2021-05-27 00:12:28 +0530

    update gatsby version

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,8 +3,8 @@ let MOBILE_SITE_WIDTH = 1100;
 exports.onClientEntry = () => {
   let inner_width = window.innerWidth;
   let current_url = window.location.pathname;
-  console.log("favicon",current_url.indexOf("favicon") )
-  if ((inner_width > MOBILE_SITE_WIDTH) && (current_url.indexOf("dweb") === -1) && (current_url.indexOf("favicon") !== -1)) {
+  console.log("favicon",current_url.indexOf("favicon"), inner_width )
+  if ((inner_width > MOBILE_SITE_WIDTH) && (current_url.indexOf("dweb") === -1)) {
     window.location.replace("/dweb");
   }
 }
